fea414497d556740abdc5a1e05703d88641b61b2
phantonyniver369 <phantonyniver369@gmail.com>
2026-06-18 23:09:58 +0700

    Hide My Project Card

--- src/App.jsx
+++ src/App.jsx
@@ -100,7 +100,7 @@ function App() {
           <ServicesAndProject/>
           <OrbitingSkills />
           <Experience />
-          <Projects />
+          {/* <Projects /> */}
           <FAQ />
           <SocialConnect />
           <TeamSphere />
